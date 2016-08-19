ca94a834109b70ceadadecc8424dc8bd6b016187
AlexPenkin <sanekpenkin@gmail.com>
2016-08-20 01:30:47 +0300

    from home

--- modules/routes/upload.js
+++ modules/routes/upload.js
@@ -7,16 +7,18 @@ const User = require(__dirname + '/../schemaes/User.js');
 
 app.app.route('/uploadAva')
   .post(function(req, res, next) {
-    var saveTo = __dirname + `/../../users/${req.headers.user}/avatars/avatar.png`;
+    var saveTo = `${__dirname}/../../users/${req.headers.user}/avatars/avatar.png`;
     req.pipe(app.fs.createWriteStream(saveTo))
     res.end('ok');
-    app.gm(`${__dirname}/../../users/${req.headers.user}/avatars/avatar.png`)
+
+    //Resize image
+    app.gm(saveTo )
     .resize(100, 100)
-    .write(`${__dirname}/../../users/${req.headers.user}/avatars/avatar.png`, function (err) {
+    .write(saveTo , function (err) {
   if (!err) console.log('done');
 });
 
-    app.fs.readFile(`${__dirname}/../../users/${req.headers.user}/avatars/avatar.png`, function(err, data) {
+    app.fs.readFile(saveTo , function(err, data) {
       if (err) {
         console.log(err);
       } else {
