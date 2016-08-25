8e97804e6a82728e212ef521b7eb16c8409990e4
AlexPenkin <sanekpenkin@gmail.com>
2016-08-26 00:11:40 +0300

    sse

--- modules/routes/groupMessage.js
+++ modules/routes/groupMessage.js
@@ -3,71 +3,81 @@ var app = require(__dirname + '/../../app.js');
 const User = require(__dirname + '/../schemaes/User.js');
 var groupInfo = {};
 var easysse = require("easysse");
+
+Array.prototype.contains = function(elem) {
+  for (var i in this) {
+    if (this[i] == elem) return true;
+  }
+  return false;
+}
 app.app.route("/notification")
-
-.get(function(req, res, next) {
-  app.notificationEmitter.on('messageNotification', function(name, message, participants) {
-    console.log('MESSAGE');
-    console.log(res.headersSent);
-    console.log(message);
-    console.log(participants);
-    if (!res.headersSent) {
-      res.writeHead(200, {
-        "Content-Type": "text/event-stream",
-        "Cache-Control": "no-cache",
-        "Connection": "keep-alive"
-      });
-    }
-    res.write("retry: 10000\n");
-    res.write("data: " + (name) + "\n\n");
-    res.write("data: " + (message) + "\n\n");
-  })
-})
-
-app.app.route('/startRoom/:roomName')
-  .get(function(req, res, next) {
-    var params = req.params.roomName;
-    var messages;
-    console.log('param: ' + req.params.roomName);
-    module.exports = groupInfo;
-
-    if (!req.user) {
-      res.redirect('/');
-    } else {
-
-      User.findOne({
-        rooms: {
-          $elemMatch: {
-            roomName: req.params.roomName
-          }
+.get(function(req, res) {
+      app.notificationEmitter.on('messageNotification', function(name, message, participants) {
+        console.log('MESSAGE');
+        console.log(participants);
+        console.log(name);
+        let members = participants.splice(participants.indexOf(name), 1)
+        console.log(participants);
+        if (members.contains(name)) {
+          if (!res.headersSent) {
+            res.writeHead(200, {
+            "Content-Type": "text/event-stream",
+            "Cache-Control": "no-cache",
+            "Connection": "keep-alive"
+          })
         }
-      }, function(err, pers) {
-        if (err) {
-          console.log(err);
-        } else {
-          for (var i = 0; i < pers.rooms.length; i++) {
-            if (pers.rooms[i].roomName == params) {
-              groupInfo.PMIDGr = pers.rooms[i].PMID;
-              groupInfo.sender = req.user.username;
-              groupInfo.participants = pers.rooms[i].participants;
-              messages = pers.rooms[i].messages;
-              res.render('groupMessagePage', {
-                user: req.user,
-                roomName: req.param.roomName,
-                messages: messages
-              });
+          res.write("retry: 10000\n");
+          res.write("data: " + (name) + "\n\n");
+          res.write("data: " + (message) + "\n\n");
+        }
+      });
+    })
 
+
+    app.app.route('/startRoom/:roomName')
+    .get(function(req, res) {
+      var params = req.params.roomName;
+      var messages;
+      console.log('param: ' + req.params.roomName);
+      module.exports = groupInfo;
+
+      if (!req.user) {
+        res.redirect('/');
+      } else {
+
+        User.findOne({
+          rooms: {
+            $elemMatch: {
+              roomName: req.params.roomName
             }
           }
-        }
-      })
+        }, function(err, pers) {
+          if (err) {
+            console.log(err);
+          } else {
+            for (var i = 0; i < pers.rooms.length; i++) {
+              if (pers.rooms[i].roomName == params) {
+                groupInfo.PMIDGr = pers.rooms[i].PMID;
+                groupInfo.sender = req.user.username;
+                groupInfo.participants = pers.rooms[i].participants;
+                messages = pers.rooms[i].messages;
+                res.render('groupMessagePage', {
+                  user: req.user,
+                  roomName: req.param.roomName,
+                  messages: messages
+                });
 
-      var gc = require(__dirname + '/../sendGroupMessage.js');
-      console.log(messages);
+              }
+            }
+          }
+        })
 
-    }
-  })
-  .post(function(req, res, next) {
+        var gc = require(__dirname + '/../sendGroupMessage.js');
+        console.log(messages);
+
+      }
+    })
+    .post(function(req, res) {
 
 
-  })
+    })
--- modules/sendGroupMessage.js
+++ modules/sendGroupMessage.js
@@ -11,62 +11,77 @@ var sockets = {};
 
 
 groupMessage.on('connection', function(socket) {
-  console.log(groupInfo);
-  sockets[groupInfo.PMIDGr] = {
-    PMID: groupInfo.PMIDGr,
-    sockets: [],
 
-  };
-  sockets[groupInfo.PMIDGr].sockets.push(socket.id);
-  socket.join(sockets[groupInfo.PMIDGr].PMID + '');
-  socket.on('sendOnServer', function(data) {
-    console.log(groupInfo);
+      sockets[groupInfo.PMIDGr] = {
+        PMID: groupInfo.PMIDGr,
+        sockets: [],
 
-    updateRoomsBd (sockets[groupInfo.PMIDGr].PMID ,data.user, data.message);
-    app.notificationEmitter.emit('messageNotification', `${data.user}`, `${data.message}, ${groupInfo.participants}`);
-    //console.log(sseSetup);
-    //groupInfo.sseSetup();
-    //groupInfo.sseSend(`Прислал сообщение: ${groupInfo.sender}`)
-    groupMessage.to(sockets[groupInfo.PMIDGr].PMID + '').emit('serverRes', `${data.user} : ${data.message}`);
-    //updateRoomsBd (sockets[groupInfo.PMIDGr].PMID ,${data.user}, ${data.message});
-    /*updateUsersWithChat(sockets[socket.id].sender, sockets[socket.id].recipient, sockets[socket.id].PMID, data.message)
-      .then(resp => console.log(resp + "dasdasd"))
-      .then(privateMessage.to(sockets[socket.id].PMID + '').emit('serverRes', `${sockets[socket.id].sender} : ${data.message}`))
-      .catch(err => console.log(err));*/
-  });
-})
+      };
 
-groupMessage.on('disconnect', function (socket) {
+      sockets[groupInfo.PMIDGr].sockets.push(socket.id);
+      socket.join(sockets[groupInfo.PMIDGr].PMID + '');
+      socket.on('sendOnServer', function(data) {
+          var members = groupInfo.participants;
+            for (let i = 0; i < members.length; i++) {
+              if (members[i] == groupInfo.sender) {
+                members.splice(i, 1);
+                return false;
+              } else {
+                console.log('Error, sender must be there');
+              }
+            }
 
-  sockets[groupInfo.PMIDG].sockets.splice(sockets[groupInfo.PMIDG].sockets.indexOf(socket.id), 1);
+            console.log(members);
+            updateRoomsBd(sockets[groupInfo.PMIDGr].PMID, data.user, data.message); app.notificationEmitter.emit(
+              'messageNotification',
+              `${data.user}`,
+              `${data.message}`,
+              members
+            );
+            //console.log(sseSetup);
+            //groupInfo.sseSetup();
+            //groupInfo.sseSend(`Прислал сообщение: ${groupInfo.sender}`)
+            groupMessage.to(sockets[groupInfo.PMIDGr].PMID + '').emit('serverRes', `${data.user} : ${data.message}`);
+            //updateRoomsBd (sockets[groupInfo.PMIDGr].PMID ,${data.user}, ${data.message});
+            /*updateUsersWithChat(sockets[socket.id].sender, sockets[socket.id].recipient, sockets[socket.id].PMID, data.message)
+              .then(resp => console.log(resp + "dasdasd"))
+              .then(privateMessage.to(sockets[socket.id].PMID + '').emit('serverRes', `${sockets[socket.id].sender} : ${data.message}`))
+              .catch(err => console.log(err));*/
+          });
+      });
 
-    console.log('disconnect');
-  });
+
+    groupMessage.on('disconnect', function(socket) {
+
+      sockets[groupInfo.PMIDG].sockets.splice(sockets[groupInfo.PMIDG].sockets.indexOf(socket.id), 1);
+
+      console.log('disconnect');
+    });
 
 
-  function updateRoomsBd (pmid ,sender, message) {
-    User.update({
-      rooms: {
-        $elemMatch: {
-          PMID: pmid
+    function updateRoomsBd(pmid, sender, message) {
+      User.update({
+        rooms: {
+          $elemMatch: {
+            PMID: pmid
+          }
         }
-      }
-    }, {
-      $push: {
-        'rooms.$.messages': {
-          name: sender,
-          message: message,
-          date: Date.now(),
-          readed: true
+      }, {
+        $push: {
+          'rooms.$.messages': {
+            name: sender,
+            message: message,
+            date: Date.now(),
+            readed: true
+          }
         }
-      }
-    }, {
-      multi: true
-    }, function(err, num) {
-      if (err) {
-        throw new Error(err);
-      }
+      }, {
+        multi: true
+      }, function(err, num) {
+        if (err) {
+          throw new Error(err);
+        }
 
-      console.log('YEAH!' + num);
-    })
-  }
+        console.log('YEAH!' + num);
+      })
+    }
